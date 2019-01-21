b33ed3d5070477b2475429d4c01a6fc62033a431
Nikos Misofis <nikomiso92@gmail.com>
2019-01-21 13:41:07 +0000

    Excercese 7 Finishing CRUD: Edit/Update Destroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,4 +22,25 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  
+  def edit
+    @movie = Movie.find params[:id]
+  end
+   
+  def update
+    @movie = Movie.find params[:id]
+    params.require(:movie)
+    permitted = params[:movie].permit(:title,:rating,:release_date)
+    @movie.update_attributes!(permitted)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
 end
